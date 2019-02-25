c345aed1dc46e1d42b11ecebe55a05b3f855dc04
tomney <gregory.tomney@gmail.com>
2019-02-24 22:17:34 -0500

    refactor bolg-backend to bolg

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,11 +1,11 @@
 # Build the docker image via:
-docker build -t gcr.io/bolg-229922/bolg-backend .
+docker build -t gcr.io/bolg-229922/bolg .
 
 # RUNNING LOCALLY CAN BE DONE VIA
-# docker run -d -p 8080:80 gcr.io/bolg-229922/bolg-backend:latest
+# docker run -d -p 8080:80 gcr.io/bolg-229922/bolg:latest
 
 # Push this docker image to the container registry via
-gcloud docker -- push gcr.io/bolg-229922/bolg-backend
+gcloud docker -- push gcr.io/bolg-229922/bolg
 
 # Use kubectl to deploy resources to the cluster for the backend:
 kubectl create -f backend.yaml
--- backend/takedown.sh
+++ backend/takedown.sh
@@ -1,5 +1,5 @@
 # Delete the backend deployment
-kubectl delete deployment bolg-backend
+kubectl delete deployment bolg
 
 # Delete the backend service
-kubectl delete service bolg-backend
+kubectl delete service bolg
