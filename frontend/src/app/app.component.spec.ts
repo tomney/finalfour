3bac463ba0c433b780d3699b8129c0061b6f3a62
tomney <gregory.tomney@gmail.com>
2019-02-25 11:28:21 -0500

    fix angular test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-finalfour!');
+    expect(compiled.querySelector('h1').textContent).toContain('to angular-finalfour!');
   }));
 });
