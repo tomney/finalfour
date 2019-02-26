39fc8b3082981e79a39cae96b47feb94e423938b
tomney <gregory.tomney@gmail.com>
2019-02-26 14:47:35 -0500

    fix dockerfile to have the correct package. Generalize api handler system on the backend

--- backend/app/app.go
+++ backend/app/app.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/tomney/angular-go-webapp/backend/app/handler"
+	"github.com/tomney/angular-go-webapp/backend/app/selections"
 )
 
 //Tentative main
@@ -22,14 +23,16 @@ func main() {
 	}
 
 	r := mux.NewRouter()
-	r.Methods("GET").Path("/api/v1/hello").Handler(appHandler(helloHandler))
+	r.Methods("GET").Path("/api/v1/hello").Handler(handler.AppHandler(helloHandler))
+	r.Methods("POST").Path("/api/v1/setSelection").Handler(handler.AppHandler(selections.SubmitSelectionHandler))
+	r.Methods("GET").Path("/api/v1/setSelection").Handler(handler.AppHandler(selections.SubmitSelectionHandler))
+
 	http.Handle("/", catchAll(r))
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) *appError {
-	log.Printf("Endpoint was actually called. I know right!")
+func helloHandler(w http.ResponseWriter, r *http.Request) *handler.AppError {
 	w.Write([]byte("{\"greeting\":\"Bienvenidos\"}"))
 	w.WriteHeader(200)
 	return nil
@@ -38,29 +41,4 @@ func helloHandler(w http.ResponseWriter, r *http.Request) *appError {
 func catchAll(r *mux.Router) http.Handler {
 	log.Printf("Well this shit isnt working but at least you are catching results")
 	return handlers.CombinedLoggingHandler(os.Stderr, r)
-}
-
-type appHandler func(http.ResponseWriter, *http.Request) *appError
-
-type appError struct {
-	Error   error
-	Message string
-	Code    int
-}
-
-func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if e := fn(w, r); e != nil { // e is *appError, not os.Error.
-		log.Printf("Handler error: status code: %d, message: %s, underlying err: %#v",
-			e.Code, e.Message, e.Error)
-
-		http.Error(w, e.Message, e.Code)
-	}
-}
-
-func appErrorf(err error, format string, v ...interface{}) *appError {
-	return &appError{
-		Error:   err,
-		Message: fmt.Sprintf(format, v...),
-		Code:    500,
-	}
 }
