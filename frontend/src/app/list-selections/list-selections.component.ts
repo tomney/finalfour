39a7f4789c54f2c5348bec99519b6d805233d6ee
tomney <gregory.tomney@gmail.com>
2019-03-20 16:47:56 -0400

    convert selections response from server to frontend representation

--- frontend/src/app/list-selections/list-selections.component.ts
+++ frontend/src/app/list-selections/list-selections.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FinalFourSelection } from '../final-four-selection';
 import { FinalFourService } from '../final-four.service';
+import { Team } from '../team';
 
 @Component({
   selector: 'app-list-selections',
@@ -13,14 +14,38 @@ export class ListSelectionsComponent implements OnInit {
   constructor(private finalFourService: FinalFourService) { }
 
   ngOnInit() {
+    this.selections = []
     this.listSelections()
   }
 
   listSelections(): void {
     this.finalFourService.listSelections().subscribe(
-      data => console.log(data)
+      response => this.convertSelectionsResponse(response)
     )
+    console.log(this.selections);
   }
 
+  //TODO move this to a more angular-approps locash
+  private convertSelectionsResponse(selections: any[]): void {
+    selections.forEach(selectionResponse => {
+        let selection: FinalFourSelection = new FinalFourSelection;
+        selection.email = selectionResponse.Email;
+        selection.teams = this.convertTeamsResponse(selectionResponse.Teams)
+        this.selections.push(selection)
+    });
+  }
 
+  //TODO move this to a more angular-approps locash
+  private convertTeamsResponse(teamsResponse: any[]): Team[] {
+    let teams: Team[] = [];
+    teamsResponse.forEach(teamResponse => {
+      let team: Team = new Team;
+      team.id = teamResponse.ID;
+      team.imageUrl = teamResponse.ImageURL;
+      team.name = teamResponse.Name;
+      teams.push(team)
+    });
+  
+    return teams;
+  }
 }
